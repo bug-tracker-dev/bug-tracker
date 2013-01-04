e54c67b558ad287b1c15d7f3dcce29603cbd4562
ZhaoSen <longzhao007@gmail.com>
2013-01-04 23:38:03 +0800

    userService and its impl

---
+++ bt-service/src/main/java/com/abc/bt/modules/sample/service/IUserService.java
@@ -0,0 +1,23 @@
+package com.abc.bt.modules.sample.service;
+
+import java.util.List;
+
+import com.abc.bt.common.model.Page;
+import com.abc.bt.modules.sample.model.User;
+
+public interface IUserService {
+	
+	/**
+	 * 查找所有用户列表
+	 * @return
+	 */
+	List<User> findAll();
+	
+	void save(User user);
+	
+	public void saveUserError(User user);
+	
+	public void update(User user);
+	
+	public Page<User> findUserByPage(Page<User> page);
+}
