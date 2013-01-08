992fd1183d4595622f5e0d0356a819fab5ae2a63
enoy <zgdx521@gmail.com>
2013-01-08 20:32:02 +0800

    add exception handler for JSON & XML

---
+++ bt-gui/src/main/java/com/abc/bt/common/web/handler/exception/BTMappingExceptionResolver.java
@@ -0,0 +1,46 @@
+package com.abc.bt.common.web.handler.exception;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+public class BTMappingExceptionResolver extends SimpleMappingExceptionResolver {
+
+	private Log _LOG = LogFactory.getLog(getClass());
+	
+	@Override
+	protected ModelAndView doResolveException(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex) {
+		
+		String accept = request.getHeader("Accept");
+		_LOG.info(accept);
+		
+		ModelAndView mav = null;
+		Integer statusCode = null;
+		if (accept.contains(MediaType.APPLICATION_XML_VALUE)) {
+			statusCode = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+			mav = new ModelAndView("xml-view");
+			mav.addObject(new Exception(ex));
+		} else if (accept.contains(MediaType.APPLICATION_JSON_VALUE)) {
+			statusCode = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+			mav = new ModelAndView("json-view");
+			mav.addObject(new Exception(ex));
+		} else {
+			String viewName = determineViewName(ex, request);
+			if (viewName != null) {
+				statusCode = determineStatusCode(request, viewName);
+				mav = getModelAndView(viewName, ex, request);
+			}
+		}
+		
+		applyStatusCodeIfPossible(request, response, statusCode);
+		
+		return mav;
+	}
+
+}
--- bt-gui/src/test/java/com/abc/bt/modules/sample/controller/SampleControllerTester2.java
+++ bt-gui/src/test/java/com/abc/bt/modules/sample/controller/SampleControllerTester2.java
@@ -12,45 +12,104 @@ import com.abc.bt.common.test.GUICommonTester2;
 
 public class SampleControllerTester2 extends GUICommonTester2 {
 
-	private static final Logger _LOG = Logger.getLogger(SampleControllerTester2.class);
+	private static final Logger _LOG = Logger
+			.getLogger(SampleControllerTester2.class);
 
-	
 	/**
-	 * <li>test case for {@link com.abc.bt.modules.sample.controller.SampleController#json()}  
-	 * <li>about the usage of jsonPath, plz refer to <a href="http://goessner.net/articles/JsonPath/">JSONPath</a>
+	 * <li>test case for
+	 * {@link com.abc.bt.modules.sample.controller.SampleController#json()} <li>
+	 * about the usage of jsonPath, plz refer to <a
+	 * href="http://goessner.net/articles/JsonPath/">JSONPath</a>
 	 */
 	@Test
 	public void json() {
 		try {
-			
+
 			// write your test code here. in the try|catch block
-			
-			ResultActions ra = this.mockMvc.perform(post("/sample/json").accept(MediaType.APPLICATION_JSON));
-			
+
+			ResultActions ra = this.mockMvc.perform(post("/sample/json")
+					.accept(MediaType.APPLICATION_JSON));
+
 			Object o = ra.andReturn().getResponse().getContentAsString();
 			_LOG.info(o);
-			
+
 			ra.andExpect(status().isOk())
-			  .andExpect(content().contentType("application/json;charset=UTF-8"))
-			  .andExpect(jsonPath("$.prop_1").value(1))
-			  .andExpect(jsonPath("$.prop_2").value(2))
-			  .andExpect(jsonPath("$.prop_3").value(3))
-			  .andExpect(jsonPath("$.prop_4").value(4));
-		} catch (Throwable e) {
-			_LOG.error(e.getMessage(), e);
-		}
-	}
-	
-	@Test
-	public void xml() {
-		try {
-			ResultActions ra = this.mockMvc.perform(post("/sample/xml").accept(MediaType.APPLICATION_XML));
-			Object o = ra.andReturn().getResponse().getContentAsString();
-			_LOG.info(o);
-			
+					.andExpect(
+							content().contentType(
+									"application/json;charset=UTF-8"))
+					.andExpect(jsonPath("$.prop_1").value(1))
+					.andExpect(jsonPath("$.prop_2").value(2))
+					.andExpect(jsonPath("$.prop_3").value(3))
+					.andExpect(jsonPath("$.prop_4").value(4));
 		} catch (Throwable e) {
 			_LOG.error(e.getMessage(), e);
 		}
 	}
 
+	@Test
+	public void errorjson() {
+		try {
+			ResultActions ra = this.mockMvc
+					.perform(post("/sample/error@handler").accept(
+							MediaType.APPLICATION_JSON));
+			Object o = ra.andReturn().getResponse().getContentAsString();
+			_LOG.info(o);
+
+		} catch (Throwable e) {
+			_LOG.error(e.getMessage(), e);
+		}
+	}
+
+	@Test
+	public void errorxml() {
+		try {
+			ResultActions ra = this.mockMvc.perform(post(
+					"/sample/error@handler").accept(MediaType.APPLICATION_XML));
+			Object o = ra.andReturn().getResponse().getContentAsString();
+			_LOG.info(o);
+
+		} catch (Throwable e) {
+			_LOG.error(e.getMessage(), e);
+		}
+	}
+
+	@Test
+	public void errordefault() {
+		try {
+			ResultActions ra = this.mockMvc
+					.perform(post("/sample/errordefault"));
+			Object o = ra.andReturn().getResponse().getContentAsString();
+			_LOG.info(o);
+
+		} catch (Throwable e) {
+			_LOG.error(e.getMessage(), e);
+		}
+	}
+
+	@Test
+	public void errorsimpleurljson() {
+		try {
+			ResultActions ra = this.mockMvc
+					.perform(post("/sample/error@handler").accept(
+							MediaType.APPLICATION_JSON));
+			Object o = ra.andReturn().getResponse().getContentAsString();
+			_LOG.info(o);
+
+		} catch (Throwable e) {
+			_LOG.error(e.getMessage(), e);
+		}
+	}
+
+	@Test
+	public void errorsimpleurlxml() {
+		try {
+			ResultActions ra = this.mockMvc.perform(post(
+					"/sample/error@handler").accept(MediaType.APPLICATION_XML));
+			Object o = ra.andReturn().getResponse().getContentAsString();
+			_LOG.info(o);
+
+		} catch (Throwable e) {
+			_LOG.error(e.getMessage(), e);
+		}
+	}
 }
