044f1773125b79cc0cfd495c2744313b04115af3
ZhaoSen <longzhao007@gmail.com>
2013-01-13 23:44:10 +0800

    add cache annotation and hibernate version column

--- bt-service/src/main/java/com/abc/bt/modules/sample/entity/Book.java
+++ bt-service/src/main/java/com/abc/bt/modules/sample/entity/Book.java
@@ -4,8 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 @Entity
 @Table(name = "T_Book")
+@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
 public class Book {
 	@Id
 	private long id;
--- bt-service/src/main/java/com/abc/bt/modules/sample/entity/User.java
+++ bt-service/src/main/java/com/abc/bt/modules/sample/entity/User.java
@@ -1,16 +1,25 @@
 package com.abc.bt.modules.sample.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 @Entity
 @Table(name = "T_User")
+@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
 public class User {
 	@Id
 	private long id;
 	private String username;
 
+	@Version
+	private int version;
+	
 	public long getId() {
 		return id;
 	}
@@ -21,6 +30,14 @@ public class User {
 
 	public String getUsername() {
 		return username;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
 	}
 
 	public void setUsername(String username) {
