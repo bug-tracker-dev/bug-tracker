be320b430f10b320ae925baedc05995b69da51b8
ZhaoSen <longzhao007@gmail.com>
2013-01-23 21:05:54 +0800

    modify test case and add Assert to check

--- bt-service/src/test/java/com/abc/bt/common/dao/GenericDaoHibernateSupportTester.java
+++ bt-service/src/test/java/com/abc/bt/common/dao/GenericDaoHibernateSupportTester.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
@@ -31,7 +32,7 @@ import com.abc.bt.modules.sample.entity.User;
 @Transactional(readOnly=true)
 public class GenericDaoHibernateSupportTester extends SVCCommonTester {
 
-	@Resource(name = "userDao")
+	@Resource(name = IUserDao.DAO_NAME)
 	private IUserDao userDao;
 
 	private static final Logger _LOG = Logger.getLogger(GenericDaoHibernateSupportTester.class);
@@ -62,6 +63,7 @@ public class GenericDaoHibernateSupportTester extends SVCCommonTester {
 	public void findAll() {
 		List<User> users = userDao.findAll();
 		showUsers(users);
+		Assert.assertEquals(5, users.size());
 	}
 
 	@Test
@@ -85,17 +87,19 @@ public class GenericDaoHibernateSupportTester extends SVCCommonTester {
 	@Test
 	@Transactional(isolation=Isolation.DEFAULT,propagation=Propagation.REQUIRED,readOnly=false)
 	public void deleteById() {
-		save();
-		userDao.deleteById(100001L);
+		userDao.deleteById(7000001L);
 	}
 
 	@Test
 	public void excuteQuery() {
 		try {
 			ResultSet rs = userDao.executeQuery("select id,username from T_User");
+			int i=0;
 			while (rs.next()) {
 				_LOG.info("id:" + rs.getObject("id") + "   " + "username:" + rs.getObject("username"));
+				i++;
 			}
+			Assert.assertEquals(5, i);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -104,34 +108,39 @@ public class GenericDaoHibernateSupportTester extends SVCCommonTester {
 	@Test
 	public void findAllByProperties() {
 		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("id", 10001L);
-		map.put("username", "Tom");
+		map.put("id", 7000001L);
+		map.put("username", "U_1");
 		List<User> users = userDao.findAllByProperties(map);
 		showUsers(users);
+		Assert.assertEquals(1, users.size());
 	}
 
 	@Test
 	public void findAllByProperty() {
-		List<User> users = userDao.findAllByProperty("username", "Tom");
+		List<User> users = userDao.findAllByProperty("username", "U_1");
 		showUsers(users);
+		Assert.assertEquals(1, users.size());
 	}
 
 	@Test
 	public void findByHQL() {
 		List<User> users = userDao.findByHQL("from User");
 		showUsers(users);
+		Assert.assertEquals(5, users.size());
 	}
 
 	@Test
 	public void findById() {
-		User user = userDao.findById(10001L);
+		User user = userDao.findById(7000001L);
 		showUser(user);
+		Assert.assertEquals("U_1", user.getUsername());
 	}
 
 	@Test
 	public void findColumnCount() {
-		int count = userDao.findColumnCount("username", "Tom");
+		int count = userDao.findColumnCount("username", "U_2");
 		_LOG.info("COUNT:" + count);
+		Assert.assertEquals(1, count);
 	}
 
 	@Test
@@ -162,24 +171,31 @@ public class GenericDaoHibernateSupportTester extends SVCCommonTester {
 
 	@Test
 	public void selectBySqlCondition() {
-		List<User> users = userDao.selectBySqlCondition("id::=10001", "username::='Tom'");
+		List<User> users = userDao.selectBySqlCondition("id::=7000003", "username::='U_3'");
 		showUsers(users);
+		Assert.assertEquals(1,users.size());
 	}
 
 	@Test
 	public void selectBySqlConditionOr() {
-		List<User> users = userDao.selectBySqlConditionOr("username::='Tom'", "username::='Jim0'");
+		List<User> users = userDao.selectBySqlConditionOr("id::=7000002", "username::='U_1'");
 		showUsers(users);
+		Assert.assertEquals(2,users.size());
 	}
 	
 	@Test
 	public void pageQuery(){
 		Page<User> page = new Page<User>();
 		page.setCurrentPage(1);
-		page.setPageSize(5);
+		page.setPageSize(3);
 		page = userDao.pageQuery(page);
 		_LOG.info("current page:"+page.getCurrentPage()+"   page size:"+page.getPageSize());
 		showUsers(page.getResult());
+		Assert.assertEquals(1,page.getCurrentPage());
+		Assert.assertEquals(5,page.getTotalCount());
+		Assert.assertEquals(3,page.getPageSize());
+		Assert.assertEquals(3,page.getResult().size());
+		
 	}
 	
 	@Test
@@ -188,22 +204,37 @@ public class GenericDaoHibernateSupportTester extends SVCCommonTester {
 		page.setCurrentPage(1);
 		page.setPageSize(5);
 		Map<String,Object> conditionMap=new HashMap<String,Object>();
-		conditionMap.put("username", "tom");
+		conditionMap.put("username", "U_1");
 		page = userDao.pageQuery(conditionMap, page);
 		_LOG.info("current page:"+page.getCurrentPage()+"   page size:"+page.getPageSize());
 		showUsers(page.getResult());	
+		
+		Assert.assertEquals(1,page.getCurrentPage());
+		Assert.assertEquals(1,page.getTotalCount());
+		Assert.assertEquals(5,page.getPageSize());
+		Assert.assertEquals(1,page.getResult().size());
+	
 	}
 	
 	@Test
 	public void pageQueryOverLoad2(){
 		Page<User> page = new Page<User>();
 		page.setCurrentPage(1);
-		page.setPageSize(5);
+		page.setPageSize(3);
 		LinkedHashMap<String,String> orderBy=new LinkedHashMap<String,String>();
 		orderBy.put("id", "desc");
 		page = userDao.pageQuery(page, orderBy);
 		_LOG.info("current page:"+page.getCurrentPage()+"   page size:"+page.getPageSize());
 		showUsers(page.getResult());	
+		List<User> users=page.getResult();
+		Assert.assertEquals(1,page.getCurrentPage());
+		Assert.assertEquals(5,page.getTotalCount());
+		Assert.assertEquals(3,page.getPageSize());
+		Assert.assertEquals(3,page.getResult().size());
+		
+		for(int i=0;i<page.getResult().size()-1;i++){
+			Assert.assertTrue(users.get(i).getId()>users.get(i+1).getId());
+		}
 	}
 	
 	@Test
@@ -214,44 +245,68 @@ public class GenericDaoHibernateSupportTester extends SVCCommonTester {
 		LinkedHashMap<String,String> orderby=new LinkedHashMap<String,String>();
 		orderby.put("id", "desc");
 		Map<String,Object> conditionMap=new HashMap<String,Object>();
-		conditionMap.put("username", "tom");
+		conditionMap.put("username", "U_1");
 		page = userDao.pageQuery(conditionMap, page, orderby);
 		_LOG.info("current page:"+page.getCurrentPage()+"   page size:"+page.getPageSize());
 		showUsers(page.getResult());	
+		
+		List<User> users=page.getResult();
+		Assert.assertEquals(1,page.getCurrentPage());
+		Assert.assertEquals(1,page.getTotalCount());
+		Assert.assertEquals(5,page.getPageSize());
+		Assert.assertEquals(1,page.getResult().size());
+		
+		for(int i=0;i<page.getResult().size()-1;i++){
+			Assert.assertTrue(users.get(i).getId()>users.get(i+1).getId());
+		}
 	}
 	
 	@Test
 	public void pageQueryOverLoad4(){
 		Page<User> page = new Page<User>();
 		page.setCurrentPage(1);
-		page.setPageSize(5);
+		page.setPageSize(3);
 		Map<String,Object> conditionMap=new HashMap<String,Object>();
-		conditionMap.put("username", "tom");
+		conditionMap.put("username", "U");
 		page = userDao.pageQuery(conditionMap, page, true);
 		_LOG.info("current page:"+page.getCurrentPage()+"   page size:"+page.getPageSize());
 		showUsers(page.getResult());	
+		
+		Assert.assertEquals(1,page.getCurrentPage());
+		Assert.assertEquals(5,page.getTotalCount());
+		Assert.assertEquals(3,page.getPageSize());
+		Assert.assertEquals(3,page.getResult().size());
+		
 	}
 	
 	@Test
 	public void pageQueryOverLoad5(){
 		Page<User> page = new Page<User>();
 		page.setCurrentPage(1);
-		page.setPageSize(5);
+		page.setPageSize(3);
 		Map<String,Object> conditionMap=new HashMap<String,Object>();
-		conditionMap.put("username", "tom");
+		conditionMap.put("username", "U");
 		LinkedHashMap<String,String> orderby=new LinkedHashMap<String,String>();
 		orderby.put("id", "desc");
 		page = userDao.pageQuery(conditionMap, page, orderby, true);
 		_LOG.info("current page:"+page.getCurrentPage()+"   page size:"+page.getPageSize());
 		showUsers(page.getResult());	
+		List<User> users=page.getResult();
+		Assert.assertEquals(1,page.getCurrentPage());
+		Assert.assertEquals(5,page.getTotalCount());
+		Assert.assertEquals(3,page.getPageSize());
+		Assert.assertEquals(3,page.getResult().size());
+		
+		for(int i=0;i<page.getResult().size()-1;i++){
+			Assert.assertTrue(users.get(i).getId()>users.get(i+1).getId());
+		}
 	}
 	
 	@Test
 	public void loadById(){
-		//org.springframework.web.servlet.handler.SimpleUrlHandlerMapping
-		@SuppressWarnings("unused")
 		User user=userDao.loadById(7000001L);
-		//showUser(user);
+		showUser(user);
+		Assert.assertEquals("U_1", user.getUsername());		
 	}
 
 }
--- bt-service/src/test/java/com/abc/bt/modules/sample/service/SampleServiceTester.java
+++ bt-service/src/test/java/com/abc/bt/modules/sample/service/SampleServiceTester.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.abc.bt.common.model.Page;
@@ -14,12 +14,16 @@ import com.abc.bt.modules.sample.entity.User;
 
 public class SampleServiceTester extends SVCCommonTester {
 
-	private static final Logger _LOG = Logger.getLogger(SampleServiceTester.class);
+	// private static final Logger _LOG =
+	// Logger.getLogger(SampleServiceTester.class);
 
 	// TODO define 2 svc: ASvc and BSvc
 
-	@Resource(name = "userService")
+	@Resource(name = IUserService.SERVICE_NAME)
 	private IUserService userService;
+
+	@Resource(name = IBookService.SERVICE_NAME)
+	private IBookService bookService;
 
 	// TODO
 
@@ -39,6 +43,7 @@ public class SampleServiceTester extends SVCCommonTester {
 		user.setId(80001L);
 		user.setUsername("Tom");
 		userService.saveUser(user);
+		Assert.assertEquals("Tom", userService.getUserById(80001L).getUsername());
 	}
 
 	@Test
@@ -49,74 +54,80 @@ public class SampleServiceTester extends SVCCommonTester {
 			user.setId(100001L);
 			user.setUsername("Tom");
 			userService.saveUserAndRollback(user);
+			Assert.fail("the saveUserAndRollback method does not throw a exception");
 		} catch (Exception e) {
-			trace(userService.loadUsersByUserName("Tom"));
+			Assert.assertNull(userService.getUserById(100001L));
 		}
 	}
 
 	@Test
 	public void remove() {
 		User user = new User();
-		user.setId(8000001L);
+		user.setId(7000001L);
 		userService.removeUser(user);
+		Assert.assertNull(userService.getUserById(7000001L));
 	}
 
 	@Test
 	public void removeRollback() {
-		// save();
 		try {
 			User user = new User();
-			user.setId(100001L);
-			user.setUsername("Tom");
+			user.setId(7000002L);
 			userService.removeUserAndRollback(user);
+			Assert.fail("the removeRollback method does not throw a exception");
 		} catch (Exception e) {
-			trace(userService.loadUsersByUserName("Tom"));
+			Assert.assertNotNull(userService.getUserById(7000002L));
 		}
 	}
 
 	@Test
 	public void update() {
-		// save();
 		User user = new User();
-		user.setId(100001L);
+		user.setId(100002L);
 		user.setUsername("Jim");
 		userService.saveUser(user);
+		Assert.assertEquals("Jim", userService.getUserById(100002L).getUsername());
 		user.setUsername("Sam");
 		userService.updateUser(user);
+		Assert.assertEquals("Sam", userService.getUserById(100002L).getUsername());
 	}
 
 	@Test
 	public void updateAndRollBack() {
 		try {
-			// save();
+
 			User user = new User();
-			user.setId(100001L);
+			user.setId(100003L);
+			user.setUsername("Tina");
+			userService.saveUser(user);
+			Assert.assertEquals("Tina", userService.getUserById(100003L).getUsername());
 			user.setUsername("Jim");
 			userService.updateUserAndRollback(user);
+			Assert.fail("the updateUserAndRollback method does not throw a exception");
 		} catch (Exception e) {
-			trace(userService.loadUsersByUserName("Tom"));
+			Assert.assertEquals("Tina", userService.getUserById(100003L).getUsername());
 		}
 	}
 
 	@Test
 	public void get() {
-		// save();
-		User user = userService.getUserById(100001L);
-		trace(user);
+		User user = userService.getUserById(7000004L);
+		Assert.assertEquals("U_4", user.getUsername());
 	}
 
 	@Test
 	public void load() {
 		// save();
-		List<User> users = userService.loadUsersByUserName("Tom");
-		trace(users);
+		List<User> users = userService.loadUsersByUserName("U_4");
+		for (User user : users) {
+			Assert.assertEquals("U_4", user.getUsername());
+		}
 	}
 
 	@Test
 	public void exist() {
-		// save();
-		boolean flag = userService.existUserName("Tom");
-		_LOG.info("Flag = " + flag);
+		boolean flag = userService.existUserName("U_4");
+		Assert.assertTrue(flag);
 	}
 
 	@Test
@@ -134,14 +145,16 @@ public class SampleServiceTester extends SVCCommonTester {
 
 		page = userService.findUserByPage(page);
 
-		trace(page.getResult());
+		Assert.assertEquals(1, page.getCurrentPage());
+		Assert.assertEquals(5, page.getResult().size());
 
 		page.setCurrentPage(3);
 		page.setPageSize(5);
 
 		page = userService.findUserByPage(page);
 
-		trace(page.getResult());
+		Assert.assertEquals(3, page.getCurrentPage());
+		Assert.assertEquals(5, page.getResult().size());
 
 	}
 
@@ -154,22 +167,26 @@ public class SampleServiceTester extends SVCCommonTester {
 		user.setId(10011L);
 		user.setUsername("Tomcat");
 		Book book = new Book();
-		book.setId(1000001L);
+		book.setId(100011L);
 		book.setBookname("java");
 		userService.saveUserAndBook(user, book);
+
+		Assert.assertEquals("Tomcat", userService.getUserById(10011L).getUsername());
+		Assert.assertEquals("java", bookService.getBookById(100011L).getBookname());
+
 	}
 
 	/**
 	 * multi-thread operate the same data via lock
 	 */
-	@Test
+	@Test(expected = org.springframework.orm.hibernate4.HibernateOptimisticLockingFailureException.class)
 	public void lock() {
-		User user1=userService.getUserById(7000001L);
-		User user2=userService.getUserById(7000001L);
+		User user1 = userService.getUserById(7000005L);
+		User user2 = userService.getUserById(7000005L);
 		user1.setUsername("Jack");
 		userService.updateUser(user1);
 		user2.setUsername("Rose");
-		//userService.updateUser(user2);
+		userService.updateUser(user2);
 	}
 
 	/**
@@ -179,15 +196,40 @@ public class SampleServiceTester extends SVCCommonTester {
 	public void svcref() {
 		User user = new User();
 		user.setId(100031L);
-		user.setUsername("Tom");
+		user.setUsername("Tony");
 		Book book1 = new Book();
-		book1.setId(10000031L);
-		book1.setBookname("java");
-		Book book2 = new Book();
-		book2.setId(10000032L);
-		book2.setBookname("javaEE");
+		book1.setId(1000031L);
+		book1.setBookname("javascript");
 		userService.saveUserAndBook(user, book1);
-		// userService.saveUserAndBooksAndRollback(user, book1, book2);
+		Assert.assertEquals("Tony", userService.getUserById(100031L).getUsername());
+		Assert.assertEquals("javascript", bookService.getBookById(1000031L).getBookname());
+	}
+
+	/**
+	 * BSvc referenced in ASvc
+	 */
+	@Test
+	public void svcrefAndRollBack() {
+		try {
+			User user = new User();
+			user.setId(100041L);
+			user.setUsername("Edison");
+			Book book1 = new Book();
+			book1.setId(1000041L);
+			book1.setBookname("java2");
+			Book book2 = new Book();
+			book2.setId(1000042L);
+			book2.setBookname("EJB");
+			userService.saveUserAndBooksAndRollback(user, book1, book2);
+			Assert.fail("the saveUserAndBooksAndRollback method does not throw a exception");
+		} catch (Exception e) {
+			Assert.assertNull(userService.getUserById(100041L));
+			Assert.assertNull(bookService.getBookById(1000041L));
+			Assert.assertNull(bookService.getBookById(1000042L));
+		}
+
+		
+
 	}
 
 	/**
@@ -196,19 +238,17 @@ public class SampleServiceTester extends SVCCommonTester {
 	@Test
 	public void annotationtxn() {
 		User user = new User();
-		user.setId(10001L);
-		user.setUsername("Tom");
+		user.setId(100051L);
+		user.setUsername("Jimmy");
 		userService.abcUser(user);
-
-		trace(userService.loadUsersByUserName("Tom"));
-
+		Assert.assertEquals("Jimmy", userService.getUserById(100051L).getUsername());
 	}
-	
+
 	@Test
-	public void cache(){
-		User user=userService.getUserById(7000001L);
+	public void cache() {
+		User user = userService.getUserById(7000001L);
 		trace(user);
-		User user1=userService.getUserById(7000001L);
+		User user1 = userService.getUserById(7000001L);
 		trace(user1);
 	}
 }
