5bbcf92ad863cc41efe57ceebc9039fcaf1c9606
ZhaoSen <longzhao007@gmail.com>
2013-01-23 21:08:02 +0800

    modify ddl

--- bt-service/src/test/resources/db/scheme.sql
+++ bt-service/src/test/resources/db/scheme.sql
@@ -1,2 +1,2 @@
-create table T_Book (id bigint not null, bookname varchar(255),version number(5) default 0, primary key (id))
-create table T_User (id bigint not null, username archar(255),version number(5) default 0, primary key (id))
+create table T_Book (id bigint not null, bookname varchar(255),version int default 0, primary key (id))
+create table T_User (id bigint not null, username varchar(255),version int default 0, primary key (id))
